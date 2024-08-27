bd04a2d8317d8ff47806ad82973c4264f0462235
David Moser <damoser@schueler.hakzell.at>
2024-08-27 23:23:50 +0200

    feat: header, index, introduction, portfolio, skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: { 
+  theme: {
+    extend: {
+      screens: {
+        'xl2': '1500px',
+      },
+    },
     fontFamily: {
       'sans': ['Archivo', 'sans-serif'],
     },
